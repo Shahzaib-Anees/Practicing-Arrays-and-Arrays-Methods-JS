4a5a182e02d4648c5552c6eb9ba261b6e5fb717a
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-10 22:19:20 +0500

    Completion of Qno01-06

---
+++ Question01/app.js
@@ -0,0 +1,15 @@
+// An Empty Array For Student Names  
+let emptyArray = [];
+
+// Declare and initialize a strings array.
+let stringArray = ["Coffee" , "Tea" , "Cake", "Bread"];
+
+// Declare and initialize a numbers array.
+let numberArray = [1,3,432,53];
+
+
+// Declare and initialize a boolean array.
+let booleanArray = [true , false , false ,true];
+
+// Declare and initialize a mixed array.
+let mixedArray = ["Car",23,"Bike" , true , "WaterMelon" , 92];
--- index.js
+++
@@ -1,9 +0,0 @@
-document.write("<h3>Question no 09 : Initializing Color Array</h3>");
-let colorsName = ["Black", "Blue" , "Green" ,"Red" , "Pink" , "Purple"];
-console.log(colorsName.length)
-
-for(a =0 ; a < colorsName.length ; a++ ){
-    document.write("<br>");
-    document.write(colorsName[a]);
-}
-
