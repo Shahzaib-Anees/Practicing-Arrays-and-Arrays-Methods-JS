82d26429c8e5e76f7f4bdcd9169379fdbbd25efd
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-19 16:03:45 +0500

    Structuring Module

---
+++ Q07-Array-declaration-&-printng-Output/app.js
@@ -0,0 +1,21 @@
+// Question 07 
+
+let qualification_in_Pakistan = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M.Phil.", "PHD"];
+document.write(`<h4>Qualifications  in  Pakistan</h4>`);
+for (a = 0; a < qualification_in_Pakistan.length; a++) {
+    document.write(qualification_in_Pakistan[a]);
+    document.write("<br>");
+};  
+
+// Question 08 
+let student_names = ["Michael" , "John" , "Tony"];
+let student_marks = [320 , 230 ,480];
+let total_marks=500;
+document.write(`<div class="card">
+<h4>Students Numbers</h4>
+<span>Marks of ${student_names[0]} is ${student_marks[0]}. Percentage ${(student_marks[0] * 100)/total_marks}%</span>
+<span>Marks of ${student_names[1]} is ${student_marks[1]}. Percentage ${(student_marks[1] * 100)/total_marks}%</span>
+<span>Marks of ${student_names[2]} is ${student_marks[2]}. Percentage ${(student_marks[2] * 100)/total_marks}%</span>
+</div>`);
+
+document.write("<br>");
