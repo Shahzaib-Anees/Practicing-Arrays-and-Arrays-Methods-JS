3ec8d6066a41f5938e193ac3cd101257d21f1d21
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-19 16:27:22 +0500

    Q 11 Added

---
+++ Q11-Array-Slicing/app.js
@@ -0,0 +1,9 @@
+
+// Question no 11 
+let citiesList =["Karachi " , "Lahore " , "Islamabad " , "Quetta " , "Peshawar " ]
+let selectedCities = citiesList.slice(2,4);
+document.write(`<div class="card">
+<h4>Practicing Array Slice Method</h4>
+<p>Cities List : ${citiesList}</p>
+<p>Selected Cities List : ${selectedCities}</p>
+</div>`)
--- Question08,10-15/app.js
+++ Question08,10-15/app.js
@@ -1,13 +1,4 @@
 
-// Question no 11 
-let citiesList =["Karachi " , "Lahore " , "Islamabad " , "Quetta " , "Peshawar " ]
-let selectedCities = citiesList.slice(2,4);
-document.write(`<div class="card">
-<h4>Practicing Array Slice Method</h4>
-<p>Cities List : ${citiesList}</p>
-<p>Selected Cities List : ${selectedCities}</p>
-</div>`)
-
 // Question no 12 
 let arr =["This " , "is " , "my " , "Cat " ]
 let arrJoint = arr.join(" ");
